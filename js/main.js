ae6251669d248c517dd413deec7e6a3fd2874663
Administrator <Teresa.Flores@Fundacion-jala.org>
2016-01-21 22:38:28 -0400

    My Task 1 - Class (sum, min, ope, avg, max)

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 /**
  * Created by Administrator on 1/20/2016.
  */
-var numbers;
+/*var numbers;
 var doOperations=function(){
     //arguments = [4,5,6]
     numbers = arguments;
 
     console.log('Avg is:',getAvg(0,numbers.length-1))
     console.log('Sum is:',getSum(0,numbers.length-1))
-   console.log('Min is:',getMin(0,numbers.length-1))
+    console.log('Min is:',getMin(0,numbers.length-1))
     console.log('Max is:',getMax(0,numbers.length-1))
 
 };
@@ -43,4 +43,27 @@ var getMax= function(initialIndex,endIndex) {
 var getAvg = function( initialIndex,endIndex) {
     return getSum(initialIndex,endIndex)/numbers.length;
 
+};*/
+
+var Class1 = function(){
+    //properties
+    var numbers;
+    this.numbers =numbers;
+    this.doOperations=function(){
+        //arguments = [4,5,6]
+        numbers = arguments;
+        //console.log('Max is:',getMax(0,numbers.length-1))
+        console.log('Avg is:',this.getAvg(0,numbers.length-1));
+        console.log('Sum is:',this.getSum(0,numbers.length-1));
+        //console.log('Min is:',getMin(0,numbers.length-1))
+    };
+    this.getSum = function( initialIndex,endIndex) {
+        if(initialIndex === endIndex);
+        { return numbers[endIndex]};
+        return numbers[initialIndex] + this.getSum(initialIndex+1, endIndex);
+    };
+    this.getAvg = function( initialIndex,endIndex) {
+        return this.getSum(initialIndex,endIndex)/numbers.length;
+
+    };
 };
