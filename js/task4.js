f6fc4eea79a739629746bf1b1b225e3dd36f641b
Administrator <Teresa.Flores@Fundacion-jala.org>
2016-02-01 21:33:33 -0400

    Callback Functions Anonymous

---
+++ js/task4.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Administrator on 2/1/2016.
+ */
+/*
+ * Say Hello
+ * @param name
+ * @param {function}cb any function to execute
+*/
+var sayHello1 = function(name,cb){
+    //this.name = name;
+    console.log('Hello '+ name);
+    cb(name);
+};
+sayHello1('Lucy',function(name2){
+    console.log('How are you'+ name2);
+});
+
+sayHello1('Lucy',function(name3){
+    console.log('How are Age'+ name3);
+});
+
+//autoejecutable en base funtions anonimas
+(
+    function(){
+        var a=2;
+        var b=3;
+        var res= a+b;
+        console.log('Sum is:'+ res);
+    })();
+
+
+// autoejecutable
+(function(a,b){
+    var a= 1,b=3;
+    var res= a+b;
+    console.log('Sum is:'+ res);
+})();
