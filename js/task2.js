7299d0518c305e7ccb0229919825c99ddfc607e3
Administrator <Teresa.Flores@Fundacion-jala.org>
2016-02-11 15:04:59 -0400

    new branch

--- js/task2.js
+++ js/task2.js
@@ -2,7 +2,8 @@
  * Created by Administrator on 1/26/2016.
  */
 var currentDay=function()
-{ var date = new Date();
+{
+    var date = new Date();
     var day;
     if (date.getDay()==0)
         day="Sunday";
@@ -24,5 +25,70 @@ var currentDay=function()
     //day ={0:'sun',1:'mon',2:'tues'};
     console.log ('Today is:',day);
     console.log ('Current time is:',d.getHours(),':'+ d.getMinutes(),':'+d.getSeconds());
-};
+
+}
+
+var MyCapicua = function(){
+    //properties
+    var numbers;
+    //this.numbers =numbers;
+    this.isCapicua=function() {
+        //arguments = [4,5,6]
+        numbers = arguments;
+        console.log('Capicúa?', this.Capicua(numbers));}
+
+        this.Capicua=function(num) {
+            var n = num;
+
+            function reverse(n) {
+                return parseInt(n.toString().split('').reverse());
+            }
+
+            function opCapicua(n) {
+                return n.toString() === reverse(n).toString();
+            }
+
+        }
+    }
+
+
+function reverseString(cadena) {
+
+    var cad = cadena;
+    // Convertir la cadena en un array
+    var cadarray = cad.split('');
+    //reverse();
+    var i=0, newcad = [];
+    do {
+         newcad[i] = newcad[i]+ cadarray[cadena.length-1];
+          i++;
+
+    } while (i == cadena.length - 1)
+
+}
+
+function factorial(num) {
+    res = 1;
+    for (var i = 1; i <= num; i++) {
+        res *= i;
+    }
+    return res;
+}
+
+
+/*
+
+function invertir(cadena) {
+    var x = cadena.length;
+    var cadinver = "";
+
+    while (x>=0) {
+        cadinver = cadinver + cadena.charAt(x);
+        x--;
+    }
+    return cadinver;
+}
+*/
+
+
 
---
+++ js/task6.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Administrator on 2/4/2016.
+ */
+
+/**
+ *
+ * @param n this string recive the name of the player
+ */
+var Player=function(){
+    var name;
+    var posy;
+    var posx;
+    this.setName= function (n) {
+        name=n;
+    };
+    this.setPosX= function (px) {
+        posxy=px;
+    };
+    this.setPosY= function (py) {
+        posxy=py;
+    };
+    this.getName= function () {
+        return name;
+    };
+    this.getPosX= function () {
+        return posx;
+    };
+    this.getPosY= function () {
+        return posy;
+        
+    };
+};
