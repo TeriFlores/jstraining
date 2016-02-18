d270cef46a6b6917c2a1708bc2b96464c63c2ac0
Administrator <Teresa.Flores@Fundacion-jala.org>
2016-02-17 20:43:57 -0400

    Improve TableCreate(), mouseOverHandler,mouseOutHandler,clickHandler

--- js/task9.js
+++ js/task9.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2/12/2016.
  */
-    
+/*
 var TableCreate =function(n,y) {
     //body
     var body = document.getElementsByTagName("body")[0];
@@ -15,7 +15,7 @@ var TableCreate =function(n,y) {
         var row = document.createElement("tr");
 
         for (var i = 0; i < y; i++) {
-            // create element <td>
+            // create elements <td>
             var cell = document.createElement("td");
             var cellText = document.createTextNode("row "+j+", column "+i);
             cell.appendChild(cellText);
@@ -26,34 +26,68 @@ var TableCreate =function(n,y) {
     }
     //<tbody> inside the <table>
     table1.appendChild(tableBody1);
-    // put <table> in the <body>
+    // <table> in the <body>
     body.appendChild(table1);
-    // <table> border attribute
+    // <table> border
     table1.setAttribute("border", "2");
 }
 
+*/
+ var mouseOverHandler=function(e){
+     e.target.innerText= Date.now() ;// elemento html donde se lanzo el click o mouse
+     console.log('hola');
+     e.target.style.color = 'red';
+     e.target.removeEventListener('mouseover',mouseOverHandler)
+    
+     // e.target.style.bgColor = 'Yellow';
 
-/*
-*var CrearTabla =function(row, col) {
+     // alert('hola');
+     //mouseover
+     //console.log('hola');
 
- var body = document.getElementsByTagName("body")[0];
- var tabla = document.createElement("table");
- var tblBody = document.createElement("tbody");
-
- for( var i= 1; i<= row; i++){
- var tblTr = document.createElement("tr");
-
-
- for (var j = 0; j < col; j++) {
-
- var celda = document.createElement("td");
- var textoCelda = document.createTextNode("row "+i+", col "+j);
- celda.appendChild(textoCelda);
- tblTr.appendChild(celda);
  }
+var mouseOutHandler=function(e) {
+    e.target.innerText= Date.now() ;// elemento html donde se lanzo el click o mouse
+    console.log('hola');
+    e.target.style.color = 'blue';
+    e.target.removeEventListener('mouseout',mouseOutHandler)
+}
+var clickHandler=function(e) {
+    e.target.innerText= Date.now() ;// elemento html donde se lanzo el click o mouse
+    console.log('hola');
+    e.target.style.color = 'yellow';
+    e.target.removeEventListener('click',clickHandler)
+}
 
- tblBody.appendChild(tblTr);
- }
- }
+var createTable=function (size){
+    var myTable=document.createElement('table');
+  var row, cell;
 
- * */
+
+    for (var i=0; i< size; i++){
+         row =document.createElement('tr');
+        for (var j=0; j<size;j++){
+            cell=document.createElement('td');
+            cell.innerText='row'+i+'cell'+j;
+            cell.addEventListener('mouseover',mouseOverHandler );
+            cell.addEventListener('mouseout',mouseOutHandler );
+            cell.addEventListener('click',clickHandler );
+            row.appendChild(cell);
+        }
+        myTable.appendChild(row);
+    }
+
+//manipula el obj
+    var body =document.getElementsByTagName('body')[0];
+    body.appendChild(myTable);
+    myTable.setAttribute("border", "2");
+}
+
+
+window.onload =function(){
+    //recien se ejecuta esta funcion , es mejor organizado
+
+    createTable(3);
+};
+
+
